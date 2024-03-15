5c63fe24c34dd48749684f4ffd9b257c4ac42d26
Kenneth <kennethyoung.2960@gmail.com>
2024-03-15 18:52:49 -0500

    Added method to concatenate, and added breakpoints for logs

--- practiceSpread.js
+++ practiceSpread.js
@@ -1,5 +1,10 @@
 // We can use the spread operator ... to expand elements of an array into another array.
 const array1 = [1, 2, 3];
 const array2 = [...array1, 4, 5, 6];
-console.log(array2);
+console.log("Test 1:", array2);
 // Now contains [1, 2, 3, 4, 5, 6]
+
+const arrayMerge1 = [1, 2, 3, 4];
+const arrayMerge2 = [5, 6, 7, 8];
+const mergedArrays = [...arrayMerge1, ...arrayMerge2];
+console.log("Test 2:", mergedArrays)
