b1b78e4a34bddf5c713bb40cd37b42290f369b0a
Kenneth <kennethyoung.2960@gmail.com>
2024-03-15 18:48:27 -0500

    Added arrays together

---
+++ practiceSpread.js
@@ -0,0 +1,5 @@
+// We can use the spread operator ... to expand elements of an array into another array.
+const array1 = [1, 2, 3];
+const array2 = [...array1, 4, 5, 6];
+console.log(array2);
+// Now contains [1, 2, 3, 4, 5, 6]
